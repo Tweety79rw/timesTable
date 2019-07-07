e0e90b987e9a084c5414d354eb28bfea13c8121b
Tweety79rw <ryan.wilson2@email.wsu.edu>
2019-07-06 19:28:03 -0700

    added some features for controlling the animation

--- sketch.js
+++ sketch.js
@@ -3,11 +3,39 @@ let r;
 let theta;
 let mult = 0;
 let points = 200;
+let pointsInput;
+let factorInput;
+let runningCheckBox;
+let slider;
+let running = true;
 function setup() {
-  createCanvas(800, 600);
+  let can = document.getElementById('canvasContainer');
+  let canvas = createCanvas(can.clientWidth, can.clientWidth);
+  canvas.parent('#canvasContainer');
   diam = height * 0.8;
   theta = 2 * PI / points;
   r = diam/2;
+  pointsInput = createInput(points, 'number');
+  pointsInput.elt.min = 2;
+  pointsInput.elt.max = 500;
+  pointsInput.parent('#points');
+  pointsInput.input(function() {
+    points = +this.value();
+    theta = 2 * PI / points;
+  });
+  factorInput = createInput(0, 'number');
+  factorInput.parent('#factor');
+  factorInput.input(function() {
+    mult = +this.value();
+  });
+  runningCheckBox = createInput(true, 'checkbox');
+  runningCheckBox.elt.checked = true;
+  runningCheckBox.parent('#running');
+  runningCheckBox.input(function() {
+    running = this.elt.checked;
+  });
+  slider = createSlider(0, 100, 10);
+  slider.parent('#speed');
 }
 function circlePos(t) {
   return {
@@ -26,9 +54,10 @@ function draw() {
     let pos2 = circlePos(theta*(i * mult % points));
     line(pos1.x, pos1.y, pos2.x, pos2.y);
   }
-  mult+= 0.006;
-
-  if(mult == 500) {
+  if(running) {
+    mult += map(+slider.value(), 0, 100, 1/600, 1/16);
+  }
+  if(mult >= points) {
     mult = 0;
   }
 }
